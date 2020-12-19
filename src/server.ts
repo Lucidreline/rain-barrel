dfbebee4a3347d8a3e9730ed3abc24890f2eb4a4
Lucidreline <techservs@pm.me>
2020-12-18 19:58:22 -0800

    Added in config for env variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
+import config from 'config'
 
+const PORT: Number = config.get('port')
 const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
   res.send('hi there')
 })
 
-app.listen(5000, () => console.log('Server Online'))
+app.listen(PORT, () => console.log(`Server online through port ${PORT}`))
