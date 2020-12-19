7d8191c722ab0cfec98e351ed4fff3588a9bea07
Lucidreline <techservs@pm.me>
2020-12-18 21:27:16 -0800

    I can now grab weather from an API and create a IWeather object with that data.

---
+++ src/grab-weather.ts
@@ -0,0 +1,50 @@
+import fetch from 'node-fetch'
+import config from 'config'
+
+const zipcode: Number = config.get('zipcode')
+
+interface IResponce {
+	current_condition: {
+		temp_F: string
+		FeelsLikeF: string
+		cloudcover: string
+		humidity: string
+		precipMM: string
+		weatherCode: string
+		windspeedMiles: string
+		weatherDesc: { value: string }[]
+		[key: string]: any
+	}[]
+	[key: string]: any
+}
+
+interface IWeather {
+	temp_F: Number
+	feelsLikeC: Number
+	cloudCover: Number
+	humidity: Number
+	precipMM: Number
+	weatherCode: Number
+	windSpeedMPH: Number
+	weatherDesc: string
+}
+
+const grabWeather = async () => {
+	const res = await fetch(`http://wttr.in/${zipcode}?format=j1`)
+	const resJson: IResponce = await res.json()
+
+	const weather: IWeather = {
+		temp_F: parseInt(resJson.current_condition[0].temp_F),
+		feelsLikeC: parseInt(resJson.current_condition[0].FeelsLikeF),
+		cloudCover: parseInt(resJson.current_condition[0].cloudcover),
+		humidity: parseInt(resJson.current_condition[0].humidity),
+		precipMM: parseInt(resJson.current_condition[0].precipMM),
+		weatherCode: parseInt(resJson.current_condition[0].weatherCode),
+		windSpeedMPH: parseInt(resJson.current_condition[0].windspeedMiles),
+		weatherDesc: resJson.current_condition[0].weatherDesc[0].value,
+	}
+
+	console.log(weather)
+}
+
+export default grabWeather
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
 import config from 'config'
+import helmet from 'helmet'
+
+import grabWeather from './grab-weather'
 
 const PORT: Number = config.get('port')
 const app: Application = express()
 
+app.use(helmet())
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('hi there')
+  grabWeather()
+  res.send('hi there!')
 })
 
 app.listen(PORT, () => console.log(`Server online through port ${PORT}`))
